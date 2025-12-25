f5bcce45dfb6cadb8c431ebc12060a06d09e83a8
HWJ <18534968246@163.com>
2025-12-25 21:45:18 +0800

    上新demo: workflow_batch.py

---
+++ demo/workflow_batch.py
@@ -0,0 +1,28 @@
+import pce
+
+data_dir = r"./data/generators/CDKM200"
+output_dir = r"./data/workflow/consensus_results"
+
+# Multiple datasets with a single algorithm
+pce.pipelines.consensus_batch(
+    input_dir=data_dir,
+    output_dir=output_dir,
+    save_format="csv",
+    consensus_method="cspa",
+    seed=2026,
+    nRepeat=10,
+    overwrite=True
+)
+
+# # Multiple datasets and multiple algorithms
+# consensus_methods = ["cspa", "ptaal", "lwea"]
+# for method in consensus_methods:
+#     pce.pipelines.consensus_batch(
+#         input_dir=data_dir,
+#         output_dir=output_dir,
+#         save_format="csv",
+#         consensus_method=method,
+#         seed=2026,
+#         nRepeat=10,
+#         overwrite=True
+#     )
