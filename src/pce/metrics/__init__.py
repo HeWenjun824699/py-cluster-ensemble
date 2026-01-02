069a5fd7cf7d045ebc577b4107e5213f56f4ebca
HWJ <18534968246@163.com>
2026-01-02 15:57:20 +0800

    metrics_to_english

--- src/pce/metrics/__init__.py
+++ src/pce/metrics/__init__.py
@@ -1,7 +1,6 @@
 from .evaluation_single import evaluation_single
 from .evaluation_batch import evaluation_batch
 
-# 定义当用户 from pce.generators import * 时导出什么
 __all__ = [
     "evaluation_single",
     "evaluation_batch"
