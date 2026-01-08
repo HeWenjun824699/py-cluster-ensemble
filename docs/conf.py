57af51c76e9ba8d70f660302d88940338fe48411
HWJ <18534968246@163.com>
2026-01-08 21:46:54 +0800

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,23 +1,17 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
 import sys
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 project = 'PCE'
 copyright = '2026, The PCE Team'
 author = 'The PCE Team'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 templates_path = ['_templates']
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_static_path = ['_static']
 html_logo = "_static/docs.png"
 html_theme_options = {
@@ -38,7 +32,6 @@ extensions = [
     'sphinx_rtd_theme'
 ]
 autosummary_generate = True
-
 autodoc_default_options = {
     'members': True,
     'undoc-members': True,
@@ -52,10 +45,3 @@ html_theme = 'sphinx_rtd_theme'
 napoleon_numpy_docstring = True
 napoleon_use_param = True
 napoleon_use_rtype = True
-
-# 5. Mock heavy dependencies for Read the Docs
-autodoc_mock_imports = [
-    'community',
-    'pyreadr',
-    'fastcluster',
-]
